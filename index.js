3425cd0e092ac2f83bba35c22cb73553ffe004e5
tarekibnkhayer <tarekibnkhayer@gmail.com>
2023-10-20 00:30:16 +0600

    cart product delete functionality implemented via _id

--- index.js
+++ index.js
@@ -72,6 +72,13 @@ async function run() {
       const cart = req.body;
       const result = await cartCollection.insertOne(cart);
       res.send(result);
+    });
+
+    app.delete('/carts/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
     })
 
 
