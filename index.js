b22ea01735b3a4b022672ac82aa71cf78398f3e5
tarekibnkhayer <tarekibnkhayer@gmail.com>
2023-10-20 15:28:35 +0600

    some code comments for deploying

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // connect to collection  of brands:
     const brandCollection   = client.db('TechWave').collection('brandCollection');
@@ -119,8 +119,8 @@ async function run() {
     })
 
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
